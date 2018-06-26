c6325f02625293d0e6004e8a2177a91bb677de0d
Hensgrej <hensgrej@gmail.com>
2018-06-26 01:36:08 -0500

    Arreglos de Estilo, implementación Sedes con resultados en tabla, comienzo de README

--- src/data.js
+++ src/data.js
@@ -1,31 +1,26 @@
 const getCampuses = () => {
     let request = new XMLHttpRequest();
     //Live API de Laboratoria
-    request.open('GET', 'https://laboratoria-la-staging.firebaseapp.com/campuses', true);
-    request.onload = function() {
+    request.open('GET', "https://api.laboratoria.la/campuses", true);
+    request.onload = () => {
         //Pasamos los datos a un objeto Javascript
-        let data = JSON.parse(this.response);
+        let data = JSON.parse(request.response);
         if (request.status >= 200 && request.status < 400) {
             //Loop de objeto
             data.forEach(campuses => {
-                //Para cada sede, se crea un botón
-                let btn = document.createElement("BUTTON");
-                //En cada iteración, se le agregar el nombre de la sede.
-                let txt = document.createTextNode(campuses.name);
-                //Los unimos
-                btn.appendChild(txt);
-                //Unimos el botón al body del HTML
-                document.body.appendChild(btn);
-
-            });
-        } else {
-            console.log('error');
+                if (campuses.id === "cdmx") {
+                    tdCdmx.innerHTML = campuses.name;
+                } else if (campuses.id === "gdl") {
+                    tdGdl.innerHTML = campuses.name;
+                } else if (campuses.id === "lim") {
+                    tdPeru.innerHTML = campuses.name;
+                } else if (campuses.id === "scl") {
+                    tdChile.innerHTML = campuses.name;
+                } else if (campuses.id === "spl") {
+                    tdBrasil.innerHTML = campuses.name;
+                }
+            })
         }
     }
     request.send();
 }
-
-/*const getGenerations = () => {
-    let secRequest = new XMLHttpRequest();
-    secRequest.open('GET', '')
-}*/
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-let btnPrueba = document.getElementById("prueba")
-btnPrueba.addEventListener("click", () => {
+btnSedes = document.getElementById("getSedes");
+tdCdmx = document.getElementById("tableCdmx");
+tdGdl = document.getElementById("tableGdl");
+tdPeru = document.getElementById("tablePeru");
+tdChile = document.getElementById("tableChile");
+tdBrasil = document.getElementById("tableBrasil");
+tabCiudad = document.getElementById("tableCiudad");
+
+tabCiudad.style.display = "none";
+btnSedes.addEventListener("click", () => {
+    tabCiudad.style.display = "block";
     getCampuses();
-})
+});
