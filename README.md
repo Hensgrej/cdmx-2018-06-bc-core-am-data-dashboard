5b1ede5e9ffd45f43a8007f9c41f8fca5319139b
Esthela1 <esthela_salas1212@hotmail.com>
2018-07-02 15:21:58 -0500

    fork, carpeta imagenes y borre las librerias

---
+++ README.md
@@ -0,0 +1,91 @@
+# Data Dashboard
+
+Este aplicación está dirigido a Training Managers de Laboratoria. Proporciona información en tiempo real de su propia base de datos, desplegada en tablas, gráficas y porcentajes, para facilitar el acceso y manipulación de ésta información.
+
+Mediante esta aplicación el usuario podrá ver y manipular data de forma fácil y rápida, mediante pestañas dentro de la página para facilitar la navegación y evitar el uso de muchas pestañas en el navegador.
+
+## Analisis de Usuario
+
+Sabemos que nuestro usuario será Training Manager de Laboratoria, de habla hispana y con conocimientos intermedios a avanzados de computación.
+Mediante encuestas y entrevistas, sabemos que actualmente maneja data con aplicaciones que le generan frustración por falta de una buena visualización de los datos, lenta navegación y diseño poco atractivo.
+
+Asi también, determinamos que la mejor manera de mostrar esta data es mediante poco texto, números llamativos, gráficas y una buena alineación de éstas ultimas.
+
+Nuestra encuesta la pueden encontrar [aquí](https://goo.gl/forms/VIoqdTJAbr6Yt1qp1).
+
+A continuación, mostramos los resultados de esta encuesta:
+
+15 personas contestaron la encuesta, en un rango de edad de 22 a 33 años, en su mayoría mujeres.
+En su mayoria, utilizan de uno a dos programas para manejar data todos los días.
+
+### Rango de Edad
+![img](https://raw.githubusercontent.com/Hensgrej/cdmx-2018-06-bc-core-am-data-dashboard/master/Media%20README/rangoEdad.PNG "Rango de Edad")
+### Como los usuarios ven los programas que utilizan actualmente
+![img](https://raw.githubusercontent.com/Hensgrej/cdmx-2018-06-bc-core-am-data-dashboard/master/Media%20README/visualizacion-programas.PNG "Visualizacion programa actual")
+### Respuesta a pregunta "¿Cómo sería el programa ideal?"
+![img](https://raw.githubusercontent.com/Hensgrej/cdmx-2018-06-bc-core-am-data-dashboard/master/Media%20README/respuestaprogramaideal.PNG "Programa Ideal")
+
+# Sintesis del Analisis de Usuario
+Para poder recolectar datos acerca de posibles usuarios, creamos una serie de preguntas orientadas
+al uso, manejo y visualización de programas de productividad, en especial aquellos que manejan
+gran cantidad de data.
+
+Entrevistamos a 15, donde 10 fueron mujeres y 5 fueron hombres.
+Con edades desde 22 años a 33 años, hispanohablantes, con conocimiento intermedio-avanzado
+de computación. 
+
+Mediante nuestras entrevistas, pudimos rescatar los siguientes datos:
+- La mayoría de las personas que utilizan herramientas para manejar data utilizan Excel y Spreadsheets
+- Se reconoce que lo que menos les agrada es la dificultad de leer data especifica y que no esten condensadas.
+- La mayoría busca un ambiente de trabajo limpio, manejo rápido y eficaz de datos y visualización condensada.
+- Predomina la preferencia por colores claros, con dos colores principales y fondo blanco o gris.
+- Se prefiere visualizar mucha información de forma rápida, poder modificarla y rápido acceso a diferentes partes de la página.
+
+
+# Funcionalidad
+
+### Diagrama de Flujo
+![img]()
+### Razonamiento
+
+Requerimientos según los test unitarios:
+   - debería exponer función computeStudentsStats en objeto global
+   - debería exponer función computeGenerationsStats en objeto global
+   - debería exponer función sortStudents en objeto global
+   - debería exponer función filterStudents en objeto global
+
+
+Al entrar a la página:
+
+- El usuario verá una categoría llamada Sede
+- Al clickear esta categoría, podrá ver una subcategoría llamada Generaciones
+- Dentro de Generaciones, encontrará los siguientes datos de estudiantes:
+  - Nombre
+  - Email
+  - Sede
+  - Generaciones
+
+Una subcategoría llamada Stats con lo siguiente:
+  - Status
+  - Porcentaje de Completado
+  - Temario
+  - Dentro de Temario:
+      - Porcentaje de Completado del Tema especifico
+      - Porcentaje de Duracion
+      - Dentro de cada tema:
+        - Porcentaje de Completado
+        - Descripcion del Tema
+        - Duración.
+
+Al tener esto, el usuario podrá buscar items especificos dentro de Generaciones
+así como ordenar subcategorías (Ejemplo: ordenar estudiantes por nombre).
+
+Asi también, podrá filtrar las generaciones:
+    - Sede
+    - Generacion
+    - Promedio de Porcentaje de Completado
+    - Numero de estudiantes de la generacion
+
+Llenaremos todas estas categorias mediante una base de datos (api.laboratoria.la).
+Buscaremos dentro de esa base de datos lo que necesitamos para llenar estas categorias
+y que el usuario pueda filtrar los datos buscando por categorias puntuales.
---
+++ src/main.js
@@ -0,0 +1,152 @@
+// Accesing all button and table IDs
+let btnSubmit = document.getElementById('btnSizing');
+let loginSection = document.getElementById('login');
+let btnsSection = document.getElementById('btnsSedes');
+let generationTable = document.getElementById('campusesGenerationTables')
+let btnLima = document.getElementById('lima');
+let btnMexico = document.getElementById('mexico');
+let btnChile = document.getElementById('chile');
+let limaTable = document.getElementById('limaGenerationsTable');
+let mexicoTable = document.getElementById('mexicoGenerationsTable');
+let chileTable = document.getElementById('chileGenerationsTable');
+let btnLimaThird = document.getElementById('btnThirdLima');
+let btnLimaFourth = document.getElementById('btnFourthLima');
+let btnLimaFifth = document.getElementById('btnFifthLima');
+let btnMexicoThird = document.getElementById('btnThirdMexico');
+let btnMexicoFourth = document.getElementById('btnFourthMexico');
+let btnMexicoFifth = document.getElementById('btnFifthMexico');
+let btnChileThird = document.getElementById('btnThirdChile');
+let btnChileFourth = document.getElementById('btnFourthChile');
+let btnChileFifth = document.getElementById('btnFifthChile');
+let limaThirdStatsTable = document.getElementById('limaThirdStudentStatsTable');
+let limaFourthStatsTable = document.getElementById('limaFourthStudentStatsTable');
+let limaFifthStatsTable = document.getElementById('limaFifthStudentStatsTable');
+let mexicoThirdStatsTable = document.getElementById('mexicoThirdStudentStatsTable');
+let mexicoFourthStatsTable = document.getElementById('mexicoFourthStudentStatsTable');
+let mexicoFifthStatsTable = document.getElementById('mexicoFifthStudentStatsTable');
+let chileThirdStatsTable = document.getElementById('chileThirdStudentStatsTable');
+let chileFourthStatsTable = document.getElementById('chileFourthStudentStatsTable');
+let chileFifthStatsTable = document.getElementById('chileFifthStudentStatsTable');
+let limaArticle = document.getElementById('limaTables');
+let mexicoArticle = document.getElementById('mexicoTables');
+let chileArticle = document.getElementById('chileTables');
+let studentsArticle = document.getElementById('studentStatsTable');
+
+// Table position of Lima's generations and their Array Length (Total students of each generation)
+let limaFifthLength = document.getElementById('limaFifth');
+let limaFourthLength = document.getElementById('limaFourth');
+let limaThirdLength = document.getElementById('limaThird');
+
+// Table position of Mexico's generations and their Array Length (Total students of each generation)
+let mexicoFifthLength = document.getElementById('mexicoFifth');
+let mexicoFourthLength = document.getElementById('mexicoFourth');
+let mexicoThirdLength = document.getElementById('mexicoThird');
+
+// Table position of Chile's generations and their Array Length (Total students of each generation)
+let chileFifthLength = document.getElementById('chileFifth');
+let chileFourthLength = document.getElementById('chileFourth');
+let chileThirdLength = document.getElementById('chileThird');
+
+// Storing Data from data.js
+let testReturn = {};
+
+// Storing Lima's generational stats
+let limaThirdEachStudentName = [];
+let limaThirdEachStudentMail = [];
+let limaThirdStudentProgress = [];
+let limaThirdProgramLength = [];
+let limaThirdProgramPercentage = [];
+let limaFourthEachStudentName = [];
+let limaFourthEachStudentMail = [];
+let limaFourthStudentProgress = [];
+let limaFourthProgramLength = [];
+let limaFourthProgramPercentage = [];
+let limaFifthEachStudentName = [];
+let limaFifthEachStudentMail = [];
+let limaFifthStudentProgress = [];
+let limaFifthProgramLength = [];
+let limaFifthProgramPercentage = [];
+// Storing Mexico's generational stats
+let mexicoThirdEachStudentName = [];
+let mexicoThirdEachStudentMail = [];
+let mexicoThirdStudentProgress = [];
+let mexicoThirdProgramLength = [];
+let mexicoThirdProgramPercentage = [];
+let mexicoFourthEachStudentName = [];
+let mexicoFourthEachStudentMail = [];
+let mexicoFourthStudentProgress = [];
+let mexicoFourthProgramLength = [];
+let mexicoFourthProgramPercentage = [];
+let mexicoFifthEachStudentName = [];
+let mexicoFifthEachStudentMail = [];
+let mexicoFifthStudentProgress = [];
+let mexicoFifthProgramLength = [];
+let mexicoFifthProgramPercentage = [];
+// Storing Chile's generational stats
+let chileThirdEachStudentName = [];
+let chileThirdEachStudentMail = [];
+let chileThirdStudentProgress = [];
+let chileThirdProgramLength = [];
+let chileThirdProgramPercentage = [];
+let chileFourthEachStudentName = [];
+let chileFourthEachStudentMail = [];
+let chileFourthStudentProgress = [];
+let chileFourthProgramLength = [];
+let chileFourthProgramPercentage = [];
+let chileFifthEachStudentName = [];
+let chileFifthEachStudentMail = [];
+let chileFifthStudentProgress = [];
+let chileFifthProgramLength = [];
+let chileFifthProgramPercentage = [];
+// Hiding non necessary HTML elements for Initial Home Page
+generationTable.style.display = "none";
+btnsSection.style.display = "none";
+studentsArticle.style.display = "none";
+
+// Calling event function to kickstart the rest of the website
+btnSubmit.addEventListener("click", () => {
+    loginSection.style.display = "none";
+    btnsSection.style.display = "block";
+    data.computeStudentStats()
+    // If the user chooses Lima
+    btnLima.addEventListener("click", () => {
+            generationTable.style.display = "block"
+            chileTable.style.display = "none";
+            mexicoTable.style.display = "none"
+            btnChile.style.display = "none";
+            btnMexico.style.display = "none";
+            btnLima.style.display = "none";
+            limaTable.style.display = "block";
+            data.printStudentsLima();
+            btnLimaThird.addEventListener("click", () => {
+                limaTable.style.display = "none";
+                studentsArticle.style.display = "block"
+                mexicoArticle.style.display = "none";
+                chileArticle.style.display="none";
+                
+            })
+        })
+        // User chooses Mexico
+    btnMexico.addEventListener("click", () => {
+            generationTable.style.display = "block"
+            btnChile.style.display = "none";
+            btnMexico.style.display = "none";
+            btnLima.style.display = "none";
+            limaTable.style.display = "none";
+            chileTable.style.display = "none";
+            mexicoTable.style.display = "block";
+            data.printStudentsMexico();
+
+        })
+        // User chooses Chile
+    btnChile.addEventListener("click", () => {
+        generationTable.style.display = "block"
+        btnChile.style.display = "none";
+        btnMexico.style.display = "none";
+        btnLima.style.display = "none";
+        mexicoTable.style.display = "none";
+        limaTable.style.display = "none";
+        chileTable.style.display = "block";
+        data.printStudentsChile();
+    })
+});
