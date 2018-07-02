b171bb5205438d1fe5578ae4efa54ff7632eb302
Esthela1 <esthela_salas1212@hotmail.com>
2018-07-02 11:31:22 -0500

    Borrar librerias y agregar carpeta de fotos

--- src/actualdata.js
+++ src/actualdata.js
@@ -19,7 +19,83 @@ window.data = {
             let chileStudentsThirdGeneration = data.santiago.generacion.tercera.estudiantes;
             let chileStudentsFourthGeneration = data.santiago.generacion.cuarta.estudiantes;
             let chileStudentsFifthGeneration = data.santiago.generacion.quinta.estudiantes;
+            console.log(chileStudentsFifthGeneration);
             // End of Chile Students
+            // Start of Lima Student Stats
+            let limaThirdStats = data.lima.generacion.tercera.estudiantes;
+            for (let i = 0; i < limaThirdStats.length; i++) {
+                limaThirdEachStudentName = data.lima.generacion.tercera.estudiantes[i].nombre;
+                limaThirdEachStudentMail = data.lima.generacion.tercera.estudiantes[i].correo;
+                limaThirdStudentProgress = data.lima.generacion.tercera.estudiantes[i].progreso;
+                limaThirdProgramLength = data.lima.generacion.tercera.estudiantes[i].progreso.duracionPrograma;
+                limaThirdProgramPercentage = data.lima.generacion.tercera.estudiantes[i].progreso.porcentajeCompletado;
+            }   
+            let limaFourthStats = data.lima.generacion.cuarta.estudiantes;
+            for (let j = 0; j < limaFourthStats.length; j++) {
+                limaFourthEachStudentName = data.lima.generacion.cuarta.estudiantes[j].nombre;
+                limaFourthEachStudentMail = data.lima.generacion.cuarta.estudiantes[j].correo;
+                limaFourthStudentProgress = data.lima.generacion.cuarta.estudiantes[j].progreso;
+                limaFourthProgramLength = data.lima.generacion.cuarta.estudiantes[j].progreso.duracionPrograma;
+                limaFourthProgramPercentage = data.lima.generacion.cuarta.estudiantes[j].progreso.porcentajeCompletado;
+            }
+            let limaFifthStats = data.lima.generacion.quinta.estudiantes;
+            for(let k = 0; k < limaFifthStats.length; k++) {
+                limaFifthEachStudentName = data.lima.generacion.quinta.estudiantes[k].nombre;
+                limaFifthEachStudentMail = data.lima.generacion.quinta.estudiantes[k].correo;
+                limaFifthEachStudentProgress = data.lima.generacion.quinta.estudiantes[k].progreso;
+                limaFifthStudentLength = data.lima.generacion.quinta.estudiantes[k].progreso.duracionPrograma;
+                limaFifthProgramPercentage = data.lima.generacion.quinta.estudiantes[k].progreso.porcentajeCompletado;
+            }
+            // Start of Mexico Student Stats
+            let mexicoThirdStats = data.mexico.generacion.tercera.estudiantes;
+            for (let i = 0; i < mexicoThirdStats.length; i++) {
+                mexicoThirdEachStudentName = data.mexico.generacion.tercera.estudiantes[i].nombre;
+                mexicoThirdEachStudentMail = data.mexico.generacion.tercera.estudiantes[i].correo;
+                mexicoThirdStudentProgress = data.mexico.generacion.tercera.estudiantes[i].progreso;
+                mexicoThirdProgramLength = data.mexico.generacion.tercera.estudiantes[i].progreso.duracionPrograma;
+                mexicoThirdProgramPercentage = data.mexico.generacion.tercera.estudiantes[i].progreso.porcentajeCompletado;
+            }
+            let mexicoFourthStats = data.mexico.generacion.cuarta.estudiantes;
+            for (let j = 0; j < mexicoFourthStats.length; j++) {
+                mexicoFourthEachStudentName = data.mexico.generacion.cuarta.estudiantes[j].nombre;
+                mexicoFourthEachStudentMail = data.mexico.generacion.cuarta.estudiantes[j].correo;
+                mexicoFourthStudentProgress = data.mexico.generacion.cuarta.estudiantes[j].progreso;
+                mexicoFourthProgramLength = data.mexico.generacion.cuarta.estudiantes[j].progreso.duracionPrograma;
+                mexicoFourthProgramPercentage = data.mexico.generacion.cuarta.estudiantes[j].progreso.porcentajeCompletado;
+            }
+            let mexicoFifthStats = data.mexico.generacion.quinta.estudiantes;
+            for(let k = 0; k < mexicoFifthStats.length; k++) {
+                mexicoFifthEachStudentName = data.mexico.generacion.quinta.estudiantes[k].nombre;
+                mexicoFifthEachStudentMail = data.mexico.generacion.quinta.estudiantes[k].correo;
+                mexicoFifthEachStudentProgress = data.mexico.generacion.quinta.estudiantes[k].progreso;
+                mexicoFifthStudentLength = data.mexico.generacion.quinta.estudiantes[k].progreso.duracionPrograma;
+                mexicoFifthProgramPercentage = data.mexico.generacion.quinta.estudiantes[k].progreso.porcentajeCompletado;
+            }
+             // Start of Chile Student Stats
+             let chileThirdStats = data.santiago.generacion.tercera.estudiantes;
+             for (let i = 0; i < chileThirdStats.length; i++) {
+                chileThirdEachStudentName = data.santiago.generacion.tercera.estudiantes[i].nombre;
+                chileThirdEachStudentMail = data.santiago.generacion.tercera.estudiantes[i].correo;
+                chileThirdStudentProgress = data.santiago.generacion.tercera.estudiantes[i].progreso;
+                chileThirdProgramLength = data.santiago.generacion.tercera.estudiantes[i].progreso.duracionPrograma;
+                chileThirdProgramPercentage = data.santiago.generacion.tercera.estudiantes[i].progreso.porcentajeCompletado;
+             }
+             let chileFourthStats = data.santiago.generacion.cuarta.estudiantes;
+             for (let j = 0; j < chileFourthStats.length; j++) {
+                chileFourthEachStudentName = data.santiago.generacion.cuarta.estudiantes[j].nombre;
+                chileFourthEachStudentMail = data.santiago.generacion.cuarta.estudiantes[j].correo;
+                chileFourthStudentProgress = data.santiago.generacion.cuarta.estudiantes[j].progreso;
+                chileFourthProgramLength = data.santiago.generacion.cuarta.estudiantes[j].progreso.duracionPrograma;
+                chileFourthProgramPercentage = data.santiago.generacion.cuarta.estudiantes[j].progreso.porcentajeCompletado;
+             }
+             let chileFifthStats = data.santiago.generacion.quinta.estudiantes;
+             for(let k = 0; k < chileFifthStats.length; k++) {
+                chileFifthEachStudentName = data.santiago.generacion.quinta.estudiantes[k].nombre;
+                chileFifthEachStudentMail = data.santiago.generacion.quinta.estudiantes[k].correo;
+                chileFifthEachStudentProgress = data.santiago.generacion.quinta.estudiantes[k].progreso;
+                chileFifthStudentLength = data.santiago.generacion.quinta.estudiantes[k].progreso.duracionPrograma;
+                chileFifthProgramPercentage = data.santiago.generacion.quinta.estudiantes[k].progreso.porcentajeCompletado;
+             }
             testReturn = {
                 'campus': ['Lima', 'México', 'Santiago'],
                 'students': [
@@ -33,23 +109,33 @@ window.data = {
                     chileStudentsFourthGeneration,
                     chileStudentsFifthGeneration,
                 ]
+
             }
+            returnStats = {
+                students : []
+            }
+            console.log(testReturn.students[8]);
+            //console.log(testReturn);
             return testReturn;
+
         })
     },
-    printStudents: () => {
-        if (btnLima) {
+    printStudentsLima: () => {
             limaThirdLength.innerHTML = testReturn.students[0].length;
             limaFourthLength.innerHTML = testReturn.students[1].length;
-            limaFifthLength.innerHTML = testReturn.students[2].length;
-        } else if (btnMexico) {
+            limaFifthLength.innerHTML = testReturn.students[2].length
+    },
+    printStudentsMexico: () => {
             mexicoThirdLength.innerHTML = testReturn.students[3].length;
             mexicoFourthLength.innerHTML = testReturn.students[4].length;
             mexicoFifthLength.innerHTML = testReturn.students[5].length;
-        } else if (btnChile) {
+    },
+    printStudentsChile: () => {
             chileThirdLength.innerHTML = testReturn.students[6].length;
             chileFourthLength.innerHTML = testReturn.students[7].length;
             chileFifthLength.innerHTML = testReturn.students[8].length;
-        }
+        },
+    generationStats : () => {
+        let 
     }
 }
