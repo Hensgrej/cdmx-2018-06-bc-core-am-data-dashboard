b095fa9dbd5d0a4768d859afd98c77d5210ff9b6
Hensgrej <hensgrej@gmail.com>
2018-07-01 19:14:24 -0500

    funcion computeStudentStats devuelve array con campus y students

--- src/main.js
+++ src/main.js
@@ -14,25 +14,16 @@ let chileTable = document.getElementById('chileGenerationsTable');
 let limaFifthLength = document.getElementById('limaFifth');
 let limaFourthLength = document.getElementById('limaFourth');
 let limaThirdLength = document.getElementById('limaThird');
-//let { lengthLima1, lengthLima2, lengthLima3 } = data.computeStudentStats;
 
 // Table position of Mexico's generations and their Array Length (Total students of each generation)
 let mexicoFifthLength = document.getElementById('mexicoFifth');
 let mexicoFourthLength = document.getElementById('mexicoFourth');
 let mexicoThirdLength = document.getElementById('mexicoThird');
-//let { lengthMexico1, lengthMexico2, lengthMexico3 } = data.computeStudentStats;
-mexicoFifthLength = lengthMexico1;
-mexicoFourthLength = lengthMexico2;
-mexicoThirdLength = lengthMexico3;
 
 // Table position of Chile's generations and their Array Length (Total students of each generation)
 let chileFifthLength = document.getElementById('chileFifth');
 let chileFourthLength = document.getElementById('chileFourth');
 let chileThirdLength = document.getElementById('chileThird');
-//let { lengthChile1, lengthChile2, lengthChile3 } = data.computeStudentStats;
-chileFifthLength = lengthChile1;
-chileFourthLength = lengthChile2;
-chileThirdLength = lengthChile3;
 
 // Hiding non necessary HTML elements for Initial Home Page
 generationTable.style.display = "none";
@@ -43,25 +34,31 @@ btnSubmit.addEventListener("click", () => {
     loginSection.style.display = "none";
     btnsSection.style.display = "block";
     data.computeStudentStats();
+    // If the user chooses Lima
     btnLima.addEventListener("click", () => {
-        generationTable.style.display = "block"
-        chileTable.style.display = "none";
-        mexicoTable.style.display = "none"
-        btnChile.style.display = "none";
-        btnMexico.style.display = "none";
-        btnLima.style.display = "none";
-        limaTable.style.display = "block";
-    })
+            generationTable.style.display = "block"
+            chileTable.style.display = "none";
+            mexicoTable.style.display = "none"
+            btnChile.style.display = "none";
+            btnMexico.style.display = "none";
+            btnLima.style.display = "none";
+            limaTable.style.display = "block";
+        })
+        // User chooses Mexico
     btnMexico.addEventListener("click", () => {
-        btnChile.style.display = "none";
-        btnMexico.style.display = "none";
-        btnLima.style.display = "none";
-        mexicoTable.style.display = "block";
-    })
+            generationTable.style.display = "block"
+            btnChile.style.display = "none";
+            btnMexico.style.display = "none";
+            btnLima.style.display = "none";
+            mexicoTable.style.display = "block";
+        })
+        // User chooses Chile
     btnChile.addEventListener("click", () => {
+        generationTable.style.display = "block"
         btnChile.style.display = "none";
         btnMexico.style.display = "none";
         btnLima.style.display = "none";
         chileTable.style.display = "block";
+
     })
 });
