08b3150931a6f9810d32648a97ff89220d2de162
Hensgrej <hensgrej@gmail.com>
2018-07-02 20:05:01 -0500

    quitando console.logs

--- src/actualdata.js
+++ src/actualdata.js
@@ -19,7 +19,6 @@ window.data = {
             let chileStudentsThirdGeneration = data.santiago.generacion.tercera.estudiantes;
             let chileStudentsFourthGeneration = data.santiago.generacion.cuarta.estudiantes;
             let chileStudentsFifthGeneration = data.santiago.generacion.quinta.estudiantes;
-            console.log(chileStudentsFifthGeneration);
             // End of Chile Students
             // Start of Lima Student Stats
             let limaThirdStats = data.lima.generacion.tercera.estudiantes;
@@ -222,8 +221,6 @@ window.data = {
             returnStats = {
                 students: []
             }
-            console.log(testReturn.students[8]);
-            //console.log(testReturn);
             return testReturn;
 
         })
