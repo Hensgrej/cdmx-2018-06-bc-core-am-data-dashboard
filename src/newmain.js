1134a065ba5a7fc5f5d890d00bbaa259595a59d2
Hensgrej <hensgrej@gmail.com>
2018-07-10 23:01:51 -0500

    mostrando todas las estudiantes de lima

--- src/newmain.js
+++ src/newmain.js
@@ -13,6 +13,12 @@ const articleLima = document.getElementById('lima');
 const limaThirdNames = document.getElementById('limaNamesThird');
 const limaFourthNames = document.getElementById('limaNamesFourth');
 const limaFifthNames = document.getElementById('limaNamesFifth');
+const fourthLimaCompletedPercentage = document.getElementById('limaCompletedPercentageFourth');
+const limaDurationPercentageFourth = document.getElementById('limaDurationPercentageFourth');
+const thirdLimaCompletedPercentage = document.getElementById('limaCompletedPercentageThird');
+const limaDurationPercentageThird = document.getElementById('limaDurationPercentageThird');
+const fifthLimaCompletedPercentage = document.getElementById('limaCompletedPercentageFifth');
+const limaDurationPercentageFifth = document.getElementById('limaDurationPercentageFifth');
 
 window.dashboardData = {
     dataLaboratoria: (laboratoria) => {
@@ -45,16 +51,64 @@ const printStudents = (dataValues) => {
         btnMexico.style.display = 'none';
         btnStgo.style.display = 'none';
         lima.style.display = 'block';
+        let thirdLima = dataValues[0].generacion.tercera.estudiantes;
+        let thirdLimaStudents = '';
+        let thirdLimaPercentages = '';
+        let thirdLimaDuration = '';
+        for (let i = 0; i < thirdLima.length; i++) {
+            let thirdLimaNames = thirdLima[i].nombre;
+            let thirdLimaCompletedPercent = thirdLima[i].progreso.porcentajeCompletado;
+            let thirdLimaProgramDuration = thirdLima[i].progreso.duracionPrograma;
+            btnLimaThird.addEventListener('click', () => {
+                limaThirdInfo.style.display = 'block';
+                btnLimaFourth.style.display = 'none';
+                btnLimaFifth.style.display = 'none';
+                thirdLimaStudents += `<p>${thirdLimaNames}</p>`;
+                thirdLimaPercentages += `<p>${thirdLimaCompletedPercent}%</p>`;
+                thirdLimaDuration += `<p>${thirdLimaProgramDuration}</p>`;
+                limaThirdNames.innerHTML = thirdLimaStudents;
+                thirdLimaCompletedPercentage.innerHTML = thirdLimaPercentages;
+                limaDurationPercentageThird.innerHTML = thirdLimaDuration;
+            })
+        }
         let fourthLima = dataValues[0].generacion.cuarta.estudiantes;
-        let fourthLimaStudents = "";
+        let fourthLimaStudents = '';
+        let fourthLimaPercentages = '';
+        let fourthLimaDuration = '';
         for (let i = 0; i < fourthLima.length; i++) {
             let fourthLimaNames = fourthLima[i].nombre;
+            let fourthLimaCompletedPercent = fourthLima[i].progreso.porcentajeCompletado;
+            let fourthLimaProgramDuration = fourthLima[i].progreso.duracionPrograma;
             btnLimaFourth.addEventListener('click', () => {
                 limaFourthInfo.style.display = 'block';
                 btnLimaThird.style.display = 'none';
                 btnLimaFifth.style.display = 'none';
                 fourthLimaStudents += `<p>${fourthLimaNames}</p>`;
+                fourthLimaPercentages += `<p>${fourthLimaCompletedPercent}%</p>`;
+                fourthLimaDuration += `<p>${fourthLimaProgramDuration}</p>`;
                 limaFourthNames.innerHTML = fourthLimaStudents;
+                fourthLimaCompletedPercentage.innerHTML = fourthLimaPercentages;
+                limaDurationPercentageFourth.innerHTML = fourthLimaDuration;
+            })
+        }
+        let fifthLima = dataValues[0].generacion.quinta.estudiantes;
+        let fifthLimaStudents = '';
+        let fifthLimaPercentages = '';
+        let fifthLimaDuration = '';
+        for (let i = 0; i < fifthLima.length; i++) {
+            let fifthLimaNames = fifthLima[i].nombre;
+            let fifthLimaCompletedPercent = fifthLima[i].progreso.porcentajeCompletado;
+            let fifthLimaProgramDuration = fifthLima[i].progreso.duracionPrograma;
+            btnLimaFifth.addEventListener('click', () => {
+                limaFifthInfo.style.display = 'block';
+                btnLimaFourth.style.display = 'none';
+                btnLimaThird.style.display = 'none';
+                fifthLimaStudents += `<p>${fifthLimaNames}</p>`;
+                fifthLimaPercentages += `<p>${fifthLimaCompletedPercent}%</p>`;
+                fifthLimaDuration += `<p>${fifthLimaProgramDuration}</p>`;
+                limaFifthNames.innerHTML = fifthLimaStudents;
+                fifthLimaCompletedPercentage.innerHTML = fifthLimaPercentages;
+                limaDurationPercentageFifth.innerHTML = fifthLimaDuration;
             })
         }
     })
