4743a115237bcb1e6c6f83f0ea96da15cce66976
Hensgrej <hensgrej@gmail.com>
2018-08-03 10:53:09 -0500

    Se implementa registro de usuarios

--- New DataDashboard/index.js
+++ New DataDashboard/index.js
@@ -6,28 +6,32 @@ const btnDropdownSantiago = document.getElementById('campus-santiago');
 const mexicoGenerations = document.getElementById('mexico-info');
 const limaGenerations = document.getElementById('lima-info');
 const santiagoGenerations = document.getElementById('santiago-info');
-
 const mexicoGenerationBtns = document.getElementsByClassName('mexico-generations');
-// Hide non necessary HTML elements
 
+let getEmail = document.getElementById('email');
+let getPassword = document.getElementById('password');
+let btnLogin = document.getElementById('btnLogin');
+
+// Hide non necessary HTML elements
 mexicoGenerations.style.display = 'none';
 limaGenerations.style.display = 'none';
 santiagoGenerations.style.display = 'none';
 
 btnDropdownMexico.addEventListener('click', () => {
-    mexicoGenerations.style.display = 'block';
-    santiagoGenerations.style.display = 'none';
-    limaGenerations.style.display = 'none';
-})
+  mexicoGenerations.style.display = 'block';
+  santiagoGenerations.style.display = 'none';
+  limaGenerations.style.display = 'none';
+});
 
 btnDropdownLima.addEventListener('click', () => {
-    mexicoGenerations.style.display = 'none';
-    santiagoGenerations.style.display = 'none';
-    limaGenerations.style.display = 'block';
-})
+  mexicoGenerations.style.display = 'none';
+  santiagoGenerations.style.display = 'none';
+  limaGenerations.style.display = 'block';
+});
 
 btnDropdownSantiago.addEventListener('click', () => {
-    santiagoGenerations.style.display = 'block';
-    mexicoGenerations.style.display = 'none';
-    limaGenerations.style.display = 'none';
-})
+  santiagoGenerations.style.display = 'block';
+  mexicoGenerations.style.display = 'none';
+  limaGenerations.style.display = 'none';
+});
+
--- New DataDashboard/main.js
+++ New DataDashboard/main.js
@@ -1,11 +1,45 @@
 // Sidenav Trigger (Code Snippet from Materialize Docs)
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
 });
 
 // Dropdown Event (Code Snippet from Materialize Docs)
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems);
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems);
+});
+
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyCP6TyCnajMP3O1kKMrnbA89Bs9kdZZoGc',
+  authDomain: 'data-dashboard-dec96.firebaseapp.com',
+  databaseURL: 'https://data-dashboard-dec96.firebaseio.com',
+  projectId: 'data-dashboard-dec96',
+  storageBucket: 'data-dashboard-dec96.appspot.com',
+  messagingSenderId: '342531694367'
+};
+firebase.initializeApp(config);
+
+// Create User
+btnLogin.addEventListener('click', (ev) => {
+  event.preventDefault(ev);
+  let email = getEmail.value;
+  let password = getPassword.value;
+  firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+    for (let i = 0; i < password.length; i++) {
+      if (email !== '@laboratoria.la') {
+        alert('Sólo Staff de Laboratoria puede crear cuentas');
+      }
+    }
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode === 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+  });
+  firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password);
 });
